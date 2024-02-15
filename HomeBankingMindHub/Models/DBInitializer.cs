67399979acbbbae663304c071f8335fd998387a5
Ezequiel_Bosco <Ezequiel_Bosco@ITRP1114>
2024-02-15 15:26:13 -0300

    Add project files.

---
+++ HomeBankingMindHub/Models/DBInitializer.cs
@@ -0,0 +1,41 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client 
+                    { 
+                        Email = "vcoronado@gmail.com", 
+                        FirstName="Victor", 
+                        LastName="Coronado", 
+                        Password="123456"
+                    },
+                    new Client
+                    {
+                        Email = "ezequielbosco@gmail.com",
+                        FirstName = "Ezequiel",
+                        LastName = "Bosco",
+                        Password = "123456"
+                    },
+                    new Client
+                    {
+                        Email = "luca@gmail.com",
+                        FirstName = "Luca",
+                        LastName = "Corona",
+                        Password = "123456"
+                    },
+                };
+
+                context.Clients.AddRange(clients);
+
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
